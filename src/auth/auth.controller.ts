6bddd9bfe52b7e2e40ea0ac176fbbc4253815ad6
kkn1125 <xpfjrnseks1@naver.com>
2025-04-14 23:22:42 +0900

    🛠️ fix: 로그인 로직 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,39 +16,22 @@ export class AuthController {
   async login(@Body() loginDto: LoginDto) {
     const secretConfig = this.commonService.getConf('secret');
 
-    try {
-      /* 데이터베이스 우선 조회 */
-      const isVerified = await this.authService.verifyPassword(loginDto);
+    const isVerified = await this.authService.verifyPassword(loginDto);
 
-      if (!isVerified) {
-        throw new BadRequestException('invalid user informations');
-      }
-
-      const token = JWT.sign(
-        {
-          email: loginDto.email,
-        },
-        secretConfig.password,
-        { algorithm: 'HS256' },
-      );
-
-      return {
-        token,
-      };
-    } catch (error) {
-      const firebaseConfig = this.commonService.getConf('firebase');
-      const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseConfig.apiKey}`;
-      const response = await axios.post(url, {
-        ...loginDto,
-        returnSecureToken: true,
-      });
-
-      /* 비밀번호 인터셉트 + 데이터베이스 저장 */
-      await this.authService.tempLoginProcess(loginDto);
-      console.log('Database');
-      return { token: response.data.idToken as string };
+    if (!isVerified) {
+      throw new BadRequestException('invalid user informations');
     }
-    // return this.authService.create(loginDto);
-    // [x] 로그인 토큰 받고 응답하는거 해야함
+
+    const token = JWT.sign(
+      {
+        email: loginDto.email,
+      },
+      secretConfig.password,
+      { algorithm: 'HS256' },
+    );
+
+    return {
+      token,
+    };
   }
 }
